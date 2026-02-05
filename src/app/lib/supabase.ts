baabd24dab28fb07ae09f3d23c040bc0b4fe35e1
aabbell <abelalemneh68@gmail.com>
2026-02-05 14:45:12 +0300

    Add Supabase env variables

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -4,6 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
-    supabaseUrl,
-    supabaseAnon
+    supabaseUrl!,
+    supabaseAnon!
 )
